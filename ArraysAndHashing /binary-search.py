ee3107811f9b0728bb96a1bd90d61d94f0dda88e
kent <kent.nolan@gmail.com>
2023-08-05 07:05:51 +1200

    Not working yet.

---
+++ ArraysAndHashing /binary-search.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        L, R = 0, len(nums)-1
+
+        while L <= R:
+            m = (L+R) // 2
+            if nums[m] < target:
+                L = m +1
+            elif nums[m] > target:
+                R = m -1
+            else: return nums[m]
+        return -1

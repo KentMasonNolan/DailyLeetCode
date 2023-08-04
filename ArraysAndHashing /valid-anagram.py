97cc8dc55188a1480f8a26af919ac0307c16f479
kent <kent.nolan@gmail.com>
2023-08-05 06:24:56 +1200

    Updated with better solution that I can use in an interview

--- ArraysAndHashing /valid-anagram.py
+++ ArraysAndHashing /valid-anagram.py
@@ -6,8 +6,18 @@ class Solution(object):
         :rtype: bool
         """
 
-        if sorted(s) == sorted(t):
-            return True
-        return False
+        countT, countS = {}, {}
+
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            countS[s[i]] = countS.get(s[i], 0) + 1
+            countT[t[i]] = countT.get(t[i], 0) + 1
+
+        for j in countS:
+            if countS[j] != countT.get(j, 0):
+                return False
+        return True
 
 # I should do this better and not the short way.
